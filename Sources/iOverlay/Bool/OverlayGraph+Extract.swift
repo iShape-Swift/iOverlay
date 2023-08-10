e544993c8696fc2c95343fdb8475ccac6a96d637
Nail Sharipov <nailxsharipov@gmail.com>
2023-08-10 19:07:20 +0300

    code clean

--- Sources/iOverlay/Bool/OverlayGraph+Extract.swift
+++ Sources/iOverlay/Bool/OverlayGraph+Extract.swift
@@ -10,8 +10,201 @@ import iFixFloat
 
 public extension OverlayGraph {
 
-    func extractShapes(operation: Operation) {
+    func extractShapes(operation: Operation) -> [FixShape] {
+        var visited = self.filter(operation: operation)
+
+        var holes = [Contour]()
+        var shapes = [FixShape]()
+        var shapeBnds = [FixBnd]()
         
+        for i in 0..<links.count {
+            if !visited[i] {
+                let contour = self.getContour(operation: operation, index: i, visited: &visited)
+                
+                if contour.isCavity {
+                    holes.append(contour)
+                } else {
+                    shapes.append(FixShape(contour: contour.path, holes: []))
+                    shapeBnds.append(contour.boundary)
+                }
+            }
+        }
+
+        guard !holes.isEmpty else {
+            return shapes
+        }
+        
+        // find for each hole it shape
+        for hole in holes {
+            var bestDist = Int64.max
+            var bestShapeIndex = -1
+
+            for shapeIndex in 0..<shapes.count {
+
+                let shape = shapes[shapeIndex]
+                let shapeBnd = shapeBnds[shapeIndex]
+
+                if shapeBnd.isInside(hole.boundary) {
+
+                    let dist = shape.contour.getBottomVerticalDistance(p: hole.start)
+
+                    if bestDist > dist {
+                        bestDist = dist
+                        bestShapeIndex = shapeIndex
+                    }
+                }
+            }
+            
+            var bestShape = shapes[bestShapeIndex]
+            bestShape.holes.append(hole.path)
+            
+            shapes[bestShapeIndex] = bestShape
+        }
+        
+        return shapes
+    }
+    
+    private func getContour(operation: Operation, index: Int, visited: inout [Bool]) -> Contour {
+        var path = FixPath()
+        var next = index
+
+        var link = links[index]
+
+        let fillRule = FillRule(operation: operation, fill: link.fill)
+        
+        var a = link.a
+        var b = link.b
+        
+        var leftLink = link
+        
+        var newVisited = [Int]()
+
+        // find a closed tour
+        repeat {
+            newVisited.append(next)
+            path.append(a.point)
+            let node = nodes[b.index]
+            
+            if node.count == 2 {
+                next = node.other(index: next)
+            } else {
+                let isFillTop = fillRule.isFillTop(fill: link.fill)
+                let isCW = OverlayGraph.isClockwise(a: a.point, b: b.point, isTopInside: isFillTop)
+                next = self.findNearestLinkTo(target: a, center: b, ignore: next, inClockWise: isCW, visited: visited)
+                guard next >= 0 else {
+                    break
+                }
+            }
+            
+            link = links[next]
+            a = b
+            b = link.other(b)
+
+            // find leftmost and bottom link
+            if leftLink.a.point.bitPack >= link.a.point.bitPack {
+                let isSamePoint = leftLink.a.index == link.a.index
+                let isSamePointAndTurnClockWise = isSamePoint && Triangle.isClockwise(p0: link.b.point, p1: link.a.point, p2: leftLink.b.point)
+                
+                if !isSamePoint || isSamePointAndTurnClockWise {
+                    leftLink = link
+                }
+            }
+            
+        } while next != index
+
+        let isCavity = fillRule.isFillBottom(fill: leftLink.fill)
+        
+        if path.area < 0 {
+            path.reverse()
+        }
+        
+        for index in newVisited {
+            visited[index] = true
+        }
+
+        return Contour(path: path, boundary: FixBnd(points: path), start: leftLink.a.point, isCavity: isCavity)
     }
 
+    private static func isClockwise(a: FixVec, b: FixVec, isTopInside: Bool) -> Bool {
+        let isDirect = a.bitPack < b.bitPack
+
+        return xnor(isDirect, isTopInside)
+    }
+    
+    private static func xnor(_ a: Bool, _ b: Bool) -> Bool {
+        a && b || !(a || b)
+    }
+
+}
+
+private enum FillRule {
+    
+    case subject
+    case clip
+    case intersect
+    case union
+    case difference
+    case xorSubject
+    case xorClip
+
+    init(operation: Operation, fill: SegmentFill) {
+        switch operation {
+        case .subject:
+            self = .subject
+        case .clip:
+            self = .clip
+        case .intersect:
+            self = .intersect
+        case .union:
+            self = .union
+        case .difference:
+            self = .difference
+        case .xor:
+            let isSubject = fill & .bothTop == .subjectTop || fill & .bothBottom == .subjectBottom
+            if isSubject {
+                self = .xorSubject
+            } else {
+                self = .xorClip
+            }
+        }
+    }
+    
+    func isFillTop(fill: SegmentFill) -> Bool {
+        switch self {
+        case .subject:
+            return fill & SegmentFill.subjectTop == SegmentFill.subjectTop
+        case .clip:
+            return fill & SegmentFill.clipTop == SegmentFill.clipTop
+        case .intersect:
+            return fill & SegmentFill.bothTop == SegmentFill.bothTop
+        case .union:
+            return fill & SegmentFill.bothBottom == 0
+        case .difference:
+            return fill & SegmentFill.bothTop == SegmentFill.subjectTop
+        case .xorSubject:
+            return fill & SegmentFill.bothTop == SegmentFill.subjectTop
+        case .xorClip:
+            return fill & SegmentFill.bothTop == SegmentFill.clipTop
+        }
+    }
+
+    func isFillBottom(fill: SegmentFill) -> Bool {
+        switch self {
+        case .subject:
+            return fill & SegmentFill.subjectTop == SegmentFill.subjectBottom
+        case .clip:
+            return fill & SegmentFill.clipTop == SegmentFill.clipBottom
+        case .intersect:
+            return fill & SegmentFill.bothBottom == SegmentFill.bothBottom
+        case .union:
+            return fill & SegmentFill.bothTop == 0
+        case .difference:
+            return fill & SegmentFill.bothBottom == SegmentFill.subjectBottom
+        case .xorSubject:
+            return fill & SegmentFill.bothBottom == SegmentFill.subjectBottom
+        case .xorClip:
+            return fill & SegmentFill.bothBottom == SegmentFill.clipBottom
+        }
+    }
+    
 }
