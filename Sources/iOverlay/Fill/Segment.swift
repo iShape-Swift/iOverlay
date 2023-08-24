8de39c0c73771dc85b89230a4fae18c6cd1f7181
Nail Sharipov <nailxsharipov@gmail.com>
2023-08-24 09:26:10 +0300

    regroupe

--- Sources/iOverlay/Fill/Segment.swift
+++ Sources/iOverlay/Fill/Segment.swift
@@ -1,9 +1,12 @@
 //
-//  SegmentFill.swift
+//  Segment.swift
 //  
 //
-//  Created by Nail Sharipov on 28.07.2023.
+//  Created by Nail Sharipov on 11.07.2023.
 //
+
+import iFixFloat
+import iShape
 
 public typealias SegmentFill = Int
 
@@ -45,4 +48,27 @@ public extension SegmentFill {
         self & SegmentFill.clipBottom == SegmentFill.clipBottom
     }
 
+}
+
+public struct Segment {
+    
+    static let zero = Segment(i: 0, a: .zero, b: .zero, shape: 0, fill: 0)
+    
+    var edge: FixEdge { FixEdge(e0: a, e1: b) }
+
+    let i: Int                  // index in store array
+    
+    // start < end
+    public let a: FixVec        // start
+    public let b: FixVec        // end
+    public let shape: ShapeType
+    public var fill: SegmentFill
+
+    init(i: Int, a: FixVec, b: FixVec, shape: ShapeType, fill: SegmentFill) {
+        self.i = i
+        self.a = a
+        self.b = b
+        self.shape = shape
+        self.fill = fill
+    }
 }
--- Sources/iOverlay/Layout/OverlayGraph.swift
+++ Sources/iOverlay/Layout/OverlayGraph.swift
@@ -13,7 +13,6 @@ public struct OverlayGraph {
     let nodes: [OverlayNode]
     let indices: [Int]
     let links: [OverlayLink]
-
     
     init(segments: [Segment]) {
         let n = segments.count
@@ -87,6 +86,53 @@ public struct OverlayGraph {
         self.indices = indices
         self.links = links
     }
+    
+    // Finds the nearest link to a given target point.
+    func findNearestLinkTo(target: IndexPoint, center: IndexPoint, ignore: Int, inClockWise: Bool, visited: [Bool]) -> Int {
+        let node = nodes[center.index]
+
+        // find any not visited vector
+        
+        var i = node.data0
+        let last = i + node.count
+
+        var minIndex = -1
+        
+        while i < last {
+            let j = indices[i]
+            if !visited[j] && ignore != j {
+                minIndex = j
+                break
+            }
+            i += 1
+        }
+        
+        guard minIndex >= 0 else {
+            return -1
+        }
+        
+        var minVec = links[minIndex].other(center).point - center.point
+        let v0 = target.point - center.point // base vector
+        
+        // compare minVec with the rest of the vectors
+        
+        i += 1
+        while i < last {
+            let j = indices[i]
+            if !visited[j] && ignore != j {
+                let vj = links[j].other(center).point - center.point
+                
+                if v0.isCloserInRotation(to: vj, comparedTo: minVec) == inClockWise {
+                    minVec = vj
+                    minIndex = j
+                }
+                
+            }
+            i += 1
+        }
+        
+        return minIndex
+    }
 }
 
 private extension Dictionary where Key == FixVec, Value == Int {
@@ -116,4 +162,35 @@ private extension OverlayNode {
             data1 += 1
         }
     }
+}
+
+private extension FixVec {
+    
+    // v, a, b vectors are multidirectional
+    func isCloserInRotation(to a: FixVec, comparedTo b: FixVec) -> Bool {
+        let crossA = self.unsafeCrossProduct(a)
+        let crossB = self.unsafeCrossProduct(b)
+
+        guard crossA != 0 && crossB != 0 else {
+            // vectors are collinear
+            if crossA == 0 {
+                // a is opposite to self, so based on crossB
+                return crossB > 0
+            } else {
+                // b is opposite to self, so based on crossA
+                return crossA < 0
+            }
+        }
+        
+        let sameSide = crossA > 0 && crossB > 0 || crossA < 0 && crossB < 0
+        
+        guard sameSide else {
+            return crossA < 0
+        }
+
+        let crossAB = a.unsafeCrossProduct(b)
+        
+        return crossAB < 0
+    }
+
 }
